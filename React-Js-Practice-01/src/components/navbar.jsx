5630c2e91727992426c78f5d87eb3266653e3d45
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-03-16 23:37:37 +0500

    practice 01

---
+++ React-Js-Practice-01/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navbar.css'
+const Navbar = () => {
+  return (
+    <>
+        <ul className='navbar'>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    </>
+  )
+}
+
+export default Navbar
