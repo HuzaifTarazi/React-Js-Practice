b8b1a98dd6b6b99f521b2a1f84b708259923db40
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-03-25 23:54:21 +0500

    react-06

--- React-Js-Practice-06/src/components/Popular/Cards.jsx
+++ React-Js-Practice-06/src/components/Popular/Cards.jsx
@@ -9,7 +9,6 @@ const Cards = () => {
         src="https://images.unsplash.com/photo-1773332585698-cba3c91b73e4?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
-
       <CardsContent />
     </div>
   );
